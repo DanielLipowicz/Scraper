e1f4fd17ea96c05ad0af469e2119edd94dc15e25
Liniel91 <lipowiczdaniel@gmail.com>
2016-05-22 21:33:43 +0200

    First beta version done.
    Refactored code.
    Main function meaning was reduced.

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from src.DataOperation import key_words_data_procesing as data_processing
 
 
 keywords = ["Big Data", "Hurtownie danych"]
+# ------------------------------------------
 # scraping update data to MongoDb database
-
+# to configure MongoDB database go to src/databases/mongodbConfig.py
+# without that application could not work
+# ------------------------------------------
 scraping_data_operations.scrap_data(keywords)
 
 # data processing and visualisation
